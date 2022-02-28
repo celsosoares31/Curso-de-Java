3be642a94d967c39554a03f4a80facdd0f01719f
celsosoares31 <celsosoares@sapo.mz>
2022-03-01 00:47:35 +0200

    Actualizacao da classe principal para incluir a instatcia da classe tipotexto

--- cursosdejava/actividades/Principal.java
+++ cursosdejava/actividades/Principal.java
@@ -4,6 +4,7 @@
  */
 package com.cursosdejava.actividades;
 
+import com.cursosdejava.aulas.tiposdedados.TipoTexto;
 import java.util.Scanner;
 
 /**
@@ -12,11 +13,12 @@ import java.util.Scanner;
  */
 public class Principal {
     public static void main(String[] args){
-        Double troco, pesoP, alturaP;
+        Double troco, pesoP, alturaP, numP;
+        String nomeP, sobreNomeP;
         //A linha abaixo instancia o objecto da classe CalculadoraValorTrocos
-        TrocosDeVenda trocos = new TrocosDeVenda();
+        //TrocosDeVenda trocos = new TrocosDeVenda();
         Scanner in = new Scanner(System.in);
-        System.out.println("\n------------------- Calculadora de Trocos----------------------------\n");
+        /*System.out.println("\n------------------- Calculadora de Trocos----------------------------\n");
         System.out.println("Digite o valor a pagar: ");
         trocos.setValorPagar(in.nextDouble());
         System.out.println("Digite o valor passado pelo cliente ");
@@ -34,6 +36,22 @@ public class Principal {
         MassaCorporal IMC = new MassaCorporal();
         System.out.println("O seu indice the massa corporal e: " + IMC.calculaMassaCorporal(pesoP, alturaP));
         
+        System.out.println("\n------------------- Calculadora do quadrado de um numero ----------------------------\n");
+        System.out.println("Digite um numero: ");
+        numP = in.nextDouble();
+        
+        QuadradoDeNumero quadradoNum = new QuadradoDeNumero();
+        System.out.println("O Quadrado de " + numP + " e: " + quadradoNum.SQRT(numP));
+        */
+        System.out.println("\n------------------- IMPRESSAO DO NOME COMPLETO ----------------------------\n");
+        System.out.println("Digite o teu nome: ");
+        nomeP = in.nextLine();
+        System.out.println("Digite o teu sobrenome: ");
+        sobreNomeP = in.nextLine();
+        
+        TipoTexto nomeCompleto = new TipoTexto(nomeP, sobreNomeP);
+        
+        System.out.println("Ola " + nomeCompleto.getNomeCompleto());
         
         in.close();
     }

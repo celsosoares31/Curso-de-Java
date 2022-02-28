5ed3614c0d73da333d1bc99c20c6fbec6831035e
celsosoares31 <celsosoares@sapo.mz>
2022-02-28 23:59:27 +0200

    inicial commit

---
+++ cursosdejava/aulas/tiposdedados/TiposNumericos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cursosdejava.aulas.tiposdedados;
+
+/**
+ *
+ * @author celso
+ */
+public class TiposNumericos {
+    
+        Byte numeroByte = 127; // os seus valores encontram-se entre -128 ate 127
+   
+    
+}
